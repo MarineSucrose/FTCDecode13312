8fe86ca879c122299680c64644a1ed043075584c
Adi <AVMarine09@icloud.com>
2025-09-27 12:58:59 -0500

    Drivebase V1 Completed

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop1.java
@@ -0,0 +1,42 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.Range;
+
+@TeleOp(name="Teleop1", group="Linear OpMode")
+public class Teleop1 extends LinearOpMode {
+
+    //Motor Variables
+    private DcMotor leftFront, leftBack, rightFront, rightBack;
+    private double driveSensitivity = 1;
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+
+        //Hardware Maps
+        leftFront  = hardwareMap.get(DcMotor.class, "leftFront");
+        leftBack  = hardwareMap.get(DcMotor.class, "leftBack");
+        rightFront  = hardwareMap.get(DcMotor.class, "rightFront");
+        rightBack  = hardwareMap.get(DcMotor.class, "rightBack");
+
+
+        double drivePower = -gamepad1.left_stick_y;
+        double turnPower  =  gamepad1.right_stick_x;
+        double strafePower = gamepad1.left_stick_x;
+
+        double lfPower = Range.clip(drivePower + turnPower + strafePower, -driveSensitivity, driveSensitivity);
+        double rfPower = Range.clip(drivePower - turnPower - strafePower,  -driveSensitivity, driveSensitivity);
+        double lbPower = Range.clip(drivePower + turnPower - strafePower, -driveSensitivity, driveSensitivity);
+        double rbPower = Range.clip(drivePower - turnPower + strafePower, -driveSensitivity, driveSensitivity);
+
+        // Send calculated power to wheels
+        leftFront.setPower(lfPower);
+        leftBack.setPower(lbPower);
+        rightFront.setPower(rfPower);
+        rightBack.setPower(rbPower);
+    }
+}
