a4f2eab21e213bc979c8d564a9d41abfd38057a7
Adi <AVMarine09@icloud.com>
2025-09-27 16:01:50 -0500

    Fixed by addeing waitForStart function and opModeIsActive while loop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop1.java
@@ -12,31 +12,34 @@ public class Teleop1 extends LinearOpMode {
     private DcMotor leftFront, leftBack, rightFront, rightBack;
     private double driveSensitivity = 1;
 
-
     @Override
     public void runOpMode() throws InterruptedException {
 
+        waitForStart();
+        while (opModeIsActive()) {
+\
 
-        //Hardware Maps
-        leftFront  = hardwareMap.get(DcMotor.class, "leftFront");
-        leftBack  = hardwareMap.get(DcMotor.class, "leftBack");
-        rightFront  = hardwareMap.get(DcMotor.class, "rightFront");
-        rightBack  = hardwareMap.get(DcMotor.class, "rightBack");
+            //Hardware Maps
+            leftFront = hardwareMap.get(DcMotor.class, "leftFront");
+            leftBack = hardwareMap.get(DcMotor.class, "leftBack");
+            rightFront = hardwareMap.get(DcMotor.class, "rightFront");
+            rightBack = hardwareMap.get(DcMotor.class, "rightBack");
 
+            double drivePower = -gamepad1.left_stick_y;
+            double turnPower = gamepad1.right_stick_x;
+            double strafePower = gamepad1.left_stick_x;
 
-        double drivePower = -gamepad1.left_stick_y;
-        double turnPower  =  gamepad1.right_stick_x;
-        double strafePower = gamepad1.left_stick_x;
+            double lfPower = Range.clip(drivePower + turnPower + strafePower, -driveSensitivity, driveSensitivity);
+            double rfPower = Range.clip(drivePower - turnPower - strafePower, -driveSensitivity, driveSensitivity);
+            double lbPower = Range.clip(drivePower + turnPower - strafePower, -driveSensitivity, driveSensitivity);
+            double rbPower = Range.clip(drivePower - turnPower + strafePower, -driveSensitivity, driveSensitivity);
 
-        double lfPower = Range.clip(drivePower + turnPower + strafePower, -driveSensitivity, driveSensitivity);
-        double rfPower = Range.clip(drivePower - turnPower - strafePower,  -driveSensitivity, driveSensitivity);
-        double lbPower = Range.clip(drivePower + turnPower - strafePower, -driveSensitivity, driveSensitivity);
-        double rbPower = Range.clip(drivePower - turnPower + strafePower, -driveSensitivity, driveSensitivity);
+            // Send calculated power to wheels
+            leftFront.setPower(lfPower);
+            leftBack.setPower(lbPower);
+            rightFront.setPower(rfPower);
+            rightBack.setPower(rbPower);
 
-        // Send calculated power to wheels
-        leftFront.setPower(lfPower);
-        leftBack.setPower(lbPower);
-        rightFront.setPower(rfPower);
-        rightBack.setPower(rbPower);
+        }
     }
 }
