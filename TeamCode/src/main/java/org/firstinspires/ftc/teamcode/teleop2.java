fb449358c426ff14d5cf530cbbf6bfd27bca6cb4
Mia <givemiacupoftea@gmail.com>
2025-11-13 09:24:32 -0600

    changing kicker power

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop2.java
@@ -93,7 +93,7 @@ public class teleop2 extends LinearOpMode {
             //kicker
 
             while(gamepad2.x){
-                kicker.setPosition(0.5);
+                kicker.setPosition(0.75);
             }
             while(gamepad2.b){
                 kicker.setPosition(0);
