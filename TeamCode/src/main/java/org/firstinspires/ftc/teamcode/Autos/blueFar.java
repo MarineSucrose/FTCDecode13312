90d8c0e434f7b49a86cff0c6d4c9e7c0ddf02bd3
Adi <AVMarine09@icloud.com>
2026-01-08 12:22:12 -0600

    auto to be tested

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autos/blueFar.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autos/blueFar.java
@@ -1,30 +1,147 @@
 package org.firstinspires.ftc.teamcode.Autos;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.InstantFunction;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+
+// Non-RR imports
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import org.firstinspires.ftc.teamcode.RoadrunnerFiles.MecanumDrive;
 
-//field is 6x6 tiles
-//tiles are 2'x 2'
+
+
 
 @Autonomous(name="blueFar", group="Linear OpMode")
 public class blueFar extends LinearOpMode {
 
-    @Override
-    public void runOpMode() throws InterruptedException {
+    private DcMotor leftFront, leftBack, rightFront, rightBack;
+    private DcMotor shooter1, shooter2;
+    private DcMotor intakeMotor;
+    private Servo intakeBlock;
+    private Servo shootBlock;
+    private Servo pivot;
 
 
-        waitForStart();
 
 
-        while (opModeIsActive()) {
+   //CLASSES FOR ACTIONS
+   //--------------------
+   public class start implements InstantFunction {
+       @Override
+       public void run() {
+           shootBlock.setPosition(0.5);
+           pivot.setPosition(0);
+           shooter1.setPower(1);
+           shooter2.setPower(1);
+           sleep(2000);
+
+
+       }
+   }
 
 
 
-        }
+    public class shoot3Artifacts implements InstantFunction{
+        @Override
+        public void run(){
+            shootBlock.setPosition(0);
+            intakeMotor.setPower(1);
+            sleep(1000);
+            shootBlock.setPosition(0.5);
+            intakeMotor.setPower(0);
+            sleep(1000);
+
+            shootBlock.setPosition(0);
+            intakeMotor.setPower(1);
+            sleep(1000);
+            shootBlock.setPosition(0.5);
+            intakeMotor.setPower(0);
+            sleep(1000);
+
+            shootBlock.setPosition(0);
+            intakeMotor.setPower(1);
+            sleep(1000);
+            shootBlock.setPosition(0.5);
+            intakeMotor.setPower(0);
+            sleep(1000);
+
 
         }
     }
+
+
+    public class intakeActivate implements InstantFunction {
+        @Override
+        public void run() {
+            intakeMotor.setPower(1);
+        }
+    }
+
+
+    public class intakeDeactivate implements InstantFunction {
+        @Override
+        public void run() {
+            intakeMotor.setPower(0);
+        }
+    }
+
+    //-------------------
+
+
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        Pose2d beginPose = new Pose2d(-12, -72, Math.toRadians(90));
+        MecanumDrive drive = new MecanumDrive(hardwareMap, beginPose);
+
+        pivot = hardwareMap.get(Servo.class, "pivot");
+        shootBlock = hardwareMap.get(Servo.class, "shootBlock");
+        intakeBlock = hardwareMap.get(Servo.class, "intakeBlock");
+
+        shooter1 = hardwareMap.get(DcMotor.class, "shooter1");
+        shooter2 = hardwareMap.get(DcMotor.class, "shooter2");
+        intakeMotor = hardwareMap.get(DcMotor.class, "intakeMotor");
+
+
+        shooter2.setDirection(DcMotor.Direction.REVERSE);
+        intakeMotor.setDirection(DcMotor.Direction.REVERSE);
+
+
+
+
+
+//---------------------
+        waitForStart();
+
+        Action path = drive.actionBuilder(beginPose)
+                .stopAndAdd(new start())
+                .lineToYLinearHeading(-60, Math.toRadians(158.19))
+                .stopAndAdd(new shoot3Artifacts())
+                .stopAndAdd(new intakeActivate())
+                .splineTo(new Vector2d(-60, -36), Math.toRadians(180))
+                .stopAndAdd(new intakeDeactivate())
+                .splineTo(new Vector2d(-12, -60), Math.toRadians(158.19))
+                .stopAndAdd(new shoot3Artifacts())
+                .lineToX(-36)
+                .build();
+
+
+        Actions.runBlocking(new SequentialAction(path));
+
+
+        }
+
+        }
+
